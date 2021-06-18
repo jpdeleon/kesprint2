492887e2ecf9994d9277cfa59ba3b208a4ddcc6c
jpdeleon <jpdeleon.bsap@gmail.com>
2021-06-18 09:13:26 +0900

    add 1114

---
+++ allesfitter/epic1114/README.txt
@@ -0,0 +1,4 @@
+2021 June 18
+* I tried different baseline models but GP Matern32 shown the most obvious phase-folded transit in initial fit
+* Fit did not converge after using either everest or k2sff lightcurves
+* try fitting per campaign
--- allesfitter/epic3752/run.py
+++ allesfitter/epic3752/run.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import allesfitter
 
-fig = allesfitter.show_initial_guess('.')
+#fig = allesfitter.show_initial_guess('.')
 
-allesfitter.ns_fit('.')
+#allesfitter.ns_fit('.')
 allesfitter.ns_output('.')
 
 #allesfitter.mcmc_fit('.')
